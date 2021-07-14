51e03470c35cfcc3fc77040f30e612b90e8adbfd
“iyha-1999” <iyha1999end@gmail.com>
2021-07-15 07:03:18 +0900

    seed値の実装

--- src/store/ducks/map/operations.js
+++ src/store/ducks/map/operations.js
@@ -1,17 +1,39 @@
 import * as Actions from "./actions";
 
+const generateSeed = (seedNumnber) => {
+    const seed = !seedNumnber ? 88675123 : seedNumnber;
+    let x = 123456789;
+    let y = 362436069;
+    let z = 521288629;
+    let w = seed;
+  
+    // XorShift
+    let t;
+  
+    const next = () => {
+      t = x ^ (x << 11);
+      x = y;
+      y = z;
+      z = w;
+      return (w = w ^ (w >>> 19) ^ (t ^ (t >>> 8)));
+    };
+    const nextInt = (min, max) => {
+      const r = Math.abs(next());
+      return min + (r % (max + 1 - min));
+    };
+  
+    return {
+      nextInt
+    };
+};
 export const generateRandomMap = () => {
     return (dispatch, getState) => {
         const { map } = getState();
-        const generateRundomInt = (maximumNumber, minimumNumber) => {
-            return (
-              Math.floor(Math.random() * (maximumNumber + 1 - minimumNumber)) +
-              minimumNumber
-            );
-        };
+        const random = generateSeed(map.seed);
         const randomArray = map.initialArray.map((value, index) => {
             return value.map(() => {
-              return generateRundomInt(3, 0);
+                console.log(random.nextInt(0, 3));
+              return random.nextInt(0, 3);
             });
         });
         dispatch(Actions.setRandomMap({
